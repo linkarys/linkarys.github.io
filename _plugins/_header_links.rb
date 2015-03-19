d49ba3abd0375fd0675c329196d8fb01d97742b2
linkarys <linkarys@gmail.com>
2015-03-20 00:36:38 +0800

    header_link

---
+++ _plugins/_header_links.rb
@@ -0,0 +1,17 @@
+require 'redcarpet'
+require 'sanitize'
+
+# Simple converter that is probably better than RedCarpet's built in TOC id
+# generator (which ends up with things lik id="toc_1"... terrible).
+
+class Redcarpet::Render::HTML
+  def header(title, level)
+    clean_title = Sanitize.clean(title)
+      .downcase
+      .gsub(/\s+/, "-")
+      .gsub(/[^A-Za-z0-9\-_.\u4e00-\u9fa5]/, "")
+
+    return "<h#{level}><a class=\"hash-link\" href=\"##{clean_title}\">§</a> <a class=\"anchor\" name=\"#{clean_title}\"></a>#{title}</h#{level}>"
+  end
+end
+
