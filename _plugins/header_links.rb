b2e6719e789fa81516a6a5c07acb3c7e7a9f869f
linkarys <linkarys@gmail.com>
2015-03-21 14:00:56 +0800

    tweak for test

--- _plugins/header_links.rb
+++ _plugins/header_links.rb
@@ -1,12 +1,11 @@
 require 'redcarpet'
-require 'sanitize'
 
 # Simple converter that is probably better than RedCarpet's built in TOC id
 # generator (which ends up with things lik id="toc_1"... terrible).
 
 class Redcarpet::Render::HTML
   def header(title, level)
-    clean_title = Sanitize.clean(title)
+    clean_title = title
       .downcase
       .gsub(/\s+/, "-")
       .gsub(/[^A-Za-z0-9\-_.\u4e00-\u9fa5]/, "")
